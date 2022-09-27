15b049f37bd831b2a55374b7736705ceee8d6e4f
ricardo1181 <ricardo39985@gmail.com>
2022-09-27 19:39:03 -0400

    Seed data

--- models/people.js
+++ models/people.js
@@ -1,11 +1,17 @@
-const faker = require('faker');
-const people = {
-
-}
+const { faker } = require("@faker-js/faker");
+const people = [];
 for (let i = 0; i < 100; i++) {
-people.push(
-    {
-
-    }
-)
-}
+  let person = {
+    firstName: faker.name.firstName(["female", "male"][i % 2]),
+    lastName: faker.name.lastName(),
+    department: faker.commerce.department(),
+    job: faker.name.jobTitle(),
+    status: ["Online", "Offline"][i % 2],
+    position: ["Senior", "Junior"][i % 2],
+    image: faker.image.people(1234, 2345, true)
+  }
+  person.email =  faker.internet.email(person.firstName, person.lastName)
+people.push(person)
+};
+console.log(people[2]);
+module.exports=people
