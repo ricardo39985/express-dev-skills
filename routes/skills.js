71cb85e9296f62913501a16e0a3e1ad30e53510e
ricardo1181 <ricardo39985@gmail.com>
2022-09-27 17:45:59 -0400

    Add index route

--- routes/skills.js
+++ routes/skills.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const router= express.Router()
+
+
+/* GET index. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
+
+  module.exports = router;
