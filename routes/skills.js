f89327d126d9f35edfdf911bba914506a3502d4b
ricardo1181 <ricardo39985@gmail.com>
2022-09-27 18:24:08 -0400

    Add edit and show routes

--- routes/skills.js
+++ routes/skills.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router= express.Router()
+const skillsCtrl = require('../controllers/skills')
 
 
 /* GET index. */
-router.get('/', function(req, res, next) {
-    res.render('skills/index', { title: 'Express' });
-  });
+router.get('/', skillsCtrl.index);
+router.get('/:id', skillsCtrl.show);
+router.get('/edit/:id', skillsCtrl.edit);
+
 
   module.exports = router;
