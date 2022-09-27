9c98f84faced9bb8a09c29370d0c132d32f79674
ricardo1181 <ricardo39985@gmail.com>
2022-09-27 19:39:42 -0400

    require people,pass people

--- controllers/people.js
+++ controllers/people.js
@@ -1,10 +1,11 @@
+const people = require('../models/people');
 module.exports = {
   index,
   show,
   edit,
 };
 function index(req, res) {
-  res.render("people/index", { title: "All people" });
+  res.render("people/index", { title: "All people", people });
 }
 function show(req, res) {
   res.render("people/show", { title: "Details" });
