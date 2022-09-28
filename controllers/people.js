c1be6f436fb6a39bf6308ae148a8bd67676b1d5f
ricardo1181 <ricardo39985@gmail.com>
2022-09-27 20:43:56 -0400

    Refactor

--- controllers/people.js
+++ controllers/people.js
@@ -1,14 +1,17 @@
-const people = require('../models/people');
+const people = require("../models/people");
 module.exports = {
   index,
   show,
   edit,
 };
 function index(req, res) {
-  res.render("people/index", { title: "All people", people });
+  res.render("people/index", { title: "All people", people: people.getAll() });
 }
 function show(req, res) {
-  res.render("people/show", { title: "Details" });
+  res.render("people/show", {
+    title: "Details",
+    person: people.getOne(req.params.id),
+  });
 }
 function edit(req, res) {
   res.render("people/edit", { title: "Edit" });
