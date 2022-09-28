94c07d4f9922d3ac74d4e5c17ca4cdf6468873ec
ricardo1181 <ricardo39985@gmail.com>
2022-09-27 20:27:05 -0400

    Add id, uuid

--- models/people.js
+++ models/people.js
@@ -1,17 +1,19 @@
 const { faker } = require("@faker-js/faker");
+const {v4: uuidv4,
+} = require('uuid');
 const people = [];
 for (let i = 0; i < 100; i++) {
   let person = {
-    firstName: faker.name.firstName(["female", "male"][i % 2]),
+    id: uuidv4(),
+    firstName: faker.name.firstName(),
     lastName: faker.name.lastName(),
     department: faker.commerce.department(),
     job: faker.name.jobTitle(),
-    status: ["Online", "Offline"][i % 2],
-    position: ["Senior", "Junior"][i % 2],
+    status: ["Online", "Offline"][Math.floor(Math.random() * 2)],
+    position: ["Senior", "Junior"][Math.floor(Math.random() * 2)],
     image: faker.image.people(1234, 2345, true)
   }
   person.email =  faker.internet.email(person.firstName, person.lastName)
 people.push(person)
 };
-console.log(people[2]);
 module.exports=people
