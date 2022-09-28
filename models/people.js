df47a993b7691a1811063254daac972350f92c3f
ricardo1181 <ricardo39985@gmail.com>
2022-09-27 20:44:28 -0400

    Refactor Export add new method

--- models/people.js
+++ models/people.js
@@ -1,7 +1,14 @@
+module.exports = {
+  getAll,
+  getOne,
+};
+
+
 const { faker } = require("@faker-js/faker");
-const {v4: uuidv4,
-} = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 const people = [];
+
+
 for (let i = 0; i < 100; i++) {
   let person = {
     id: uuidv4(),
@@ -11,9 +18,17 @@ for (let i = 0; i < 100; i++) {
     job: faker.name.jobTitle(),
     status: ["Online", "Offline"][Math.floor(Math.random() * 2)],
     position: ["Senior", "Junior"][Math.floor(Math.random() * 2)],
-    image: faker.image.people(1234, 2345, true)
-  }
-  person.email =  faker.internet.email(person.firstName, person.lastName)
-people.push(person)
-};
-module.exports=people
+    image: faker.image.people(1234, 2345, true),
+  };
+  person.email = faker.internet.email(person.firstName, person.lastName);
+  people.push(person);
+}
+function getAll() {
+  return people;
+}
+
+function getOne(id) {
+  console.log(id)
+  console.log(people.find(person => person.id === id))
+   return people.find(person => person.id === id);
+}
