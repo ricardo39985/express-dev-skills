5c434ce57f5fc1acc0cb271ba534c951b995495e
ricardo1181 <ricardo39985@gmail.com>
2022-09-29 20:04:10 -0400

    Fix bug

--- routes/people.js
+++ routes/people.js
@@ -5,7 +5,8 @@ const peopleCtrl = require("../controllers/people");
 /* GET index. */
 router.get("/", peopleCtrl.index);
 router.get("/new", peopleCtrl.new);
-router.get("/:id", peopleCtrl.new);
+router.get("/:id", peopleCtrl.show);
 router.get("/edit/:id", peopleCtrl.edit);
+router.post("/", peopleCtrl.create);
 
 module.exports = router;
