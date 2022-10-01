71f0648a9ba46845bd7fd15f3efdfdf561c2c30b
ricardo1181 <ricardo39985@gmail.com>
2022-09-30 20:32:35 -0400

    Remove unnecessary comments

--- routes/employees.js
+++ routes/employees.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const peopleCtrl = require("../controllers/employees");
 
-/* GET index. */
 router.get("/", peopleCtrl.index);
 router.get("/new", peopleCtrl.new);
 router.get("/:id", peopleCtrl.show);
@@ -11,7 +10,5 @@ router.post("/", peopleCtrl.create);
 router.delete('/:id', peopleCtrl.delete)
 router.get("/:id/edit", peopleCtrl.edit);
 router.put("/:id", peopleCtrl.update);
-
-
 
 module.exports = router;
